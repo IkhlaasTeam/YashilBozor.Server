6c915696e8ba4a042dc536f1abdebae11c5e6b62
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-01-27 03:27:26 +0500

    implemented password hasher service

---
+++ YashilBozor.Service/Services/PasswordHasherService.cs
@@ -0,0 +1,17 @@
+﻿using YashilBozor.Service.Interfaces;
+using BC = BCrypt.Net.BCrypt;
+
+namespace YashilBozor.Service.Services;
+
+public class PasswordHasherService : IPasswordHasherService
+{
+    public string HashPassword(string password)
+    {
+        return BC.HashPassword(password);
+    }
+
+    public bool ValidatePassword(string password, string hashedPassword)
+    {
+        return BC.Verify(password, hashedPassword);
+    }
+}
