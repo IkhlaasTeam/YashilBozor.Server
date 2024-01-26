7f0ca6fd9ed8014bc5117ab7a28074f62188c04c
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-01-27 03:27:49 +0500

    implemented access token generator service

---
+++ YashilBozor.Service/Interfaces/IAccessTokenGeneratorService.cs
@@ -0,0 +1,10 @@
+﻿using YashilBozor.Domain.Entities.Users;
+
+namespace YashilBozor.Service.Interfaces;
+
+public interface IAccessTokenGeneratorService
+{
+    AccessToken GetToken(User user);
+
+    Guid GetTokenId(string accessToken);
+}
