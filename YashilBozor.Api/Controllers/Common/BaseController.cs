9a09099896f398e49858cf0ac8454581ac2fddf4
Dilafruza <dilafruzjraboyeva@gmail.com>
2024-01-27 03:57:31 +0500

    Common Controller added

---
+++ YashilBozor.Api/Controllers/Common/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YashilBozor.Api.Controllers.Common;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class BaseController : ControllerBase
+{
+}
