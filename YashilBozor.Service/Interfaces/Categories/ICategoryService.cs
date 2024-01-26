6e32f75cb3a222972a2c26d120341fc88ea94a9e
Dilafruza <dilafruzjraboyeva@gmail.com>
2024-01-27 03:35:18 +0500

    ICategoryService implemented

---
+++ YashilBozor.Service/Interfaces/Categories/ICategoryService.cs
@@ -0,0 +1,33 @@
+﻿using YashilBozor.Domain.Configurations;
+using YashilBozor.Service.DTOs.Categories;
+
+namespace YashilBozor.Service.Interfaces.Categories;
+
+public interface ICategoryService
+{
+    ValueTask<IEnumerable<CategoryForResultDto>> GetAllAsync(
+        PaginationParams @params,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<CategoryForResultDto?> GetByIdAsync(
+        Guid categoryId,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<CategoryForResultDto> CreateAsync(
+        CategoryForCreationDto userForCreationDto,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<CategoryForResultDto> UpdateAsync(
+        CategoryForUpdateDto userForUpdateDto,
+        Guid categoryId,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<CategoryForResultDto?> DeleteAsync(
+        Guid categoryId,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+}
