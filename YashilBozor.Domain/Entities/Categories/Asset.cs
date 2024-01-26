3230b2e86f337fde0d784244b59a0ee0b44fed7b
Dilafruza <dilafruzjraboyeva@gmail.com>
2024-01-27 03:46:42 +0500

    Assets are added

---
+++ YashilBozor.Domain/Entities/Categories/Asset.cs
@@ -0,0 +1,11 @@
+﻿using YashilBozor.Domain.Entities.Commons;
+
+namespace YashilBozor.Domain.Entities.Categories;
+
+public class Asset : Auditable
+{
+    public string MediaPath { get; set; }
+
+    public Guid ProductId { get; set; }
+    public Product Product { get; set; }
+}
