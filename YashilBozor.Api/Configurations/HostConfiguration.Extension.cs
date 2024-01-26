bf667b02f12e407809efca6d75b1b95fa0643c45
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-01-26 23:27:01 +0500

    created host configuration

---
+++ YashilBozor.Api/Configurations/HostConfiguration.Extension.cs
@@ -0,0 +1,111 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using System.Reflection;
+using YashilBozor.DAL.DbContexts;
+
+namespace HHD.API.Configurations;
+
+public static partial class HostConfiguration
+{
+    private static readonly ICollection<Assembly> Assemblies;
+
+    static HostConfiguration()
+    {
+        Assemblies = Assembly.GetExecutingAssembly().GetReferencedAssemblies().Select(Assembly.Load).ToList();
+        Assemblies.Add(Assembly.GetExecutingAssembly());
+    }
+
+    private static WebApplicationBuilder AddValidators(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddValidatorsFromAssemblies(Assemblies);
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddMappers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAutoMapper(Assemblies);
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddBusinessLogic(this WebApplicationBuilder builder)
+    {
+        var connection = builder.Configuration.GetConnectionString("ConnectionStrings");
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddExposers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRouting(options => options.LowercaseUrls = true);
+        builder.Services.AddControllers().AddNewtonsoftJson();
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddCors(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddCors(
+            options => { 
+                options.AddDefaultPolicy(
+                policyBuilder => 
+                { 
+                    policyBuilder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader(); 
+                }
+                ); 
+            }
+        );
+
+        return builder;
+    }
+    private static WebApplicationBuilder AddDevTools(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddLogger(this WebApplicationBuilder builder)
+    {
+        var logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .Enrich.FromLogContext()
+            .CreateLogger();
+
+        builder.Logging.ClearProviders();
+
+        builder.Logging.AddSerilog(logger);
+
+        return builder;
+    }
+
+    private static WebApplication UseExposers(this WebApplication app)
+    {
+        app.MapControllers();
+
+        return app;
+    }
+
+    private static WebApplication UseDevTools(this WebApplication app) 
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        return app;
+    }
+
+    //private static WebApplication UseCustomMiddleWare(this WebApplication app)
+    //{
+    //    app.UseMiddleware<ExceptionHandlerMiddleWare>();
+    //    return app;
+    //}
+}
---
+++ YashilBozor.Api/Configurations/HostConfiguration.cs
@@ -0,0 +1,32 @@
+﻿namespace HHD.API.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder
+            .AddLogger()
+            .AddValidators()
+            .AddMappers()
+            .AddBusinessLogic()
+            .AddExposers()
+            .AddCors()
+            .AddDevTools();
+
+        return new(builder);
+    }
+
+    public static async ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        //await app.MigrateDatabaseAsync();
+
+        app
+            .UseCors();
+        app
+            //.UseCustomMiddleWare()
+            .UseExposers()
+            .UseDevTools();
+        
+        return app;
+    }
+}
