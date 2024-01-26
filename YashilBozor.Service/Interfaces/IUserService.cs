7dfd1d60f8dd83b4e90338d79654dc1cb39f9616
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-01-27 03:22:27 +0500

    implemented user service

---
+++ YashilBozor.Service/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using YashilBozor.Domain.Entities.Users;
+
+namespace YashilBozor.Service.Interfaces;
+
+public interface IUserService
+{
+    IQueryable<User> Get(Expression<Func<User, bool>>? predicate = default, bool asNoTracking = false);
+
+    ValueTask<User?> GetByIdAsync(Guid userId, bool asNoTracking = false, CancellationToken cancellationToken = default);
+
+    //ValueTask<User> GetSystemUserAsync(bool asNoTracking = false, CancellationToken cancellationToken = default);
+
+    //Task<Guid?> GetIdByEmailAddressAsync(string emailAddress, CancellationToken cancellationToken = default);
+
+    ValueTask<User> CreateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default);
+
+    ValueTask<User> UpdateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default);
+}
---
+++ YashilBozor.Service/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using YashilBozor.DAL.IRepositories.Users;
+using YashilBozor.Domain.Entities.Users;
+using YashilBozor.Service.Interfaces;
+
+namespace YashilBozor.Service.Services;
+
+public class UserService(IUserRepository userRepository) : IUserService
+{
+    public IQueryable<User> Get(Expression<Func<User, bool>>? predicate = default, bool asNoTracking = false)
+    {
+        return userRepository.SelectAll(predicate, asNoTracking);
+    }
+
+    public ValueTask<User?> GetByIdAsync(Guid userId, bool asNoTracking = false, CancellationToken cancellationToken = default)
+    {
+        return userRepository.SelectByIdAsync(userId, asNoTracking, cancellationToken);
+    }
+
+    //public async ValueTask<User> GetSystemUserAsync(bool asNoTracking = false, CancellationToken cancellationToken = default)
+    //{
+    //    return await Get(user => user.Role == RoleType.System, asNoTracking).FirstAsync(cancellationToken);
+    //}
+
+    public async Task<Guid?> GetIdByEmailAddressAsync(string emailAddress, CancellationToken cancellationToken = default)
+    {
+        var userId = await Get(user => user.EmailAddress == emailAddress, true).Select(user => user.Id).FirstOrDefaultAsync(cancellationToken);
+        return userId != Guid.Empty ? userId : default(Guid?);
+    }
+
+    public ValueTask<User> CreateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        return userRepository.InsertAsync(user, saveChanges, cancellationToken);
+    }
+
+    public ValueTask<User> UpdateAsync(User user, bool saveChanges = true, CancellationToken cancellationToken = default)
+    {
+        return userRepository.UpdateAsync(user, saveChanges, cancellationToken);
+    }
+}
