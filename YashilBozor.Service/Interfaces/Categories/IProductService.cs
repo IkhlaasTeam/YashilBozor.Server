59a061f573c9508eb91ada4656f47f1a9c9b0adf
Dilafruza <dilafruzjraboyeva@gmail.com>
2024-01-27 03:36:12 +0500

    IProductServiceImplemented

---
+++ YashilBozor.Service/Interfaces/Categories/IProductService.cs
@@ -0,0 +1,33 @@
+﻿using YashilBozor.Domain.Configurations;
+using YashilBozor.Service.DTOs.Categories.Products;
+
+namespace YashilBozor.Service.Interfaces.Categories;
+
+public interface IProductService
+{
+    ValueTask<IEnumerable<ProductForResultDto>> GetAllAsync(
+       PaginationParams @params,
+       bool asNoTracking = false,
+       CancellationToken cancellationToken = default);
+
+    ValueTask<ProductForResultDto?> GetByIdAsync(
+        Guid productId,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<ProductForResultDto> CreateAsync(
+        ProductForCreationDto productForCreationDto,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<ProductForResultDto> UpdateAsync(
+        ProductForUpdateDto productForUpdateDto,
+        Guid productId,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<ProductForResultDto?> DeleteAsync(
+        Guid productId,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+}
