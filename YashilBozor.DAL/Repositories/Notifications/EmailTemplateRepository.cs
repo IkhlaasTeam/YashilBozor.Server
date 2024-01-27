5749da2385300fb7ec3dbe6c5ea66f9d93c494e7
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-01-27 05:52:47 +0500

    Created repositories for notifications

---
+++ YashilBozor.DAL/Repositories/Notifications/EmailTemplateRepository.cs
@@ -0,0 +1,14 @@
+﻿using YashilBozor.DAL.DbContexts;
+using YashilBozor.DAL.IRepositories.Notifications;
+using YashilBozor.DAL.Repositories.Common;
+using YashilBozor.Domain.Entities.Notification;
+
+namespace YashilBozor.DAL.Repositories.Notifications;
+
+public class EmailTemplateRepository : Repository<EmailTemplate>, IEmailTemplateRepository
+{
+    public EmailTemplateRepository(AppDbContext dbContext) : base(dbContext)
+    {
+
+    }
+}
