513cc30c19a987d4533cf561ed6136b7e38a3bcb
Dilafruza <dilafruzjraboyeva@gmail.com>
2024-01-27 06:11:09 +0500

    Product asset related DTOs are done

---
+++ YashilBozor.Service/DTOs/Categories/Assets/ProductAssets/ProductAssetForCreationDto.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace YashilBozor.Service.DTOs.Categories.Assets.ProductAssets;
+
+public class ProductAssetForCreationDto
+{
+    public IFormFile FormFile { get; set; }
+    public Guid ProductId { get; set; }
+}
---
+++ YashilBozor.Service/DTOs/Categories/Assets/ProductAssets/ProductAssetForResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace YashilBozor.Service.DTOs.Categories.Assets.ProductAssets;
+
+public class ProductAssetForResultDto
+{
+    public Guid Id { get; set; }
+    public string MediaPath { get; set; }
+    public Guid ProductId { get; set; }
+}
