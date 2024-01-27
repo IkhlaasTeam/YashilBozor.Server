6af13cde0526b61ef2c5e43fa66e5dfa058f73f6
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-01-27 05:51:40 +0500

    Implemented Email sender service

---
+++ YashilBozor.Service/Interfaces/Notifications/Services/IEmailSenderService.cs
@@ -0,0 +1,8 @@
+﻿using YashilBozor.Service.Services.Notifications.Models;
+
+namespace YashilBozor.Service.Interfaces.Notifications.Services;
+
+public interface IEmailSenderService
+{
+    Task<bool> SendEmailAsync(EmailMessage emailMessage);
+}
---
+++ YashilBozor.Service/Services/Notifications/Services/EmailSenderService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+using YashilBozor.Service.Commons.Settings;
+using YashilBozor.Service.Interfaces.Notifications.Services;
+using YashilBozor.Service.Services.Notifications.Models;
+
+namespace YashilBozor.Service.Services.Notifications.Services;
+
+public class EmailSenderService(IOptions<SmtpSettings> options) : IEmailSenderService
+{
+    public SmtpClient SmtpClientInstance { get; init; }
+
+    public Task<bool> SendEmailAsync(EmailMessage emailMessage)
+    {
+        return Task.Run(async () =>
+        {
+            var result = true;
+            try
+            {
+                var smtp = new SmtpClient(options.Value.Host, options.Value.Port);
+                smtp.Credentials = new NetworkCredential(options.Value.UserName, options.Value.Password);
+                smtp.EnableSsl = true;
+
+                var mail = new MailMessage(options.Value.EmailAddress, emailMessage.ReceiverAddress);
+                mail.Subject = emailMessage.Subject;
+                mail.Body = emailMessage.Body;
+
+                emailMessage.IsSent = result;
+                emailMessage.SentTime = DateTime.UtcNow;
+
+                await smtp.SendMailAsync(mail);
+            }
+            catch (Exception e)
+            {
+                result = false;
+            }
+
+            return result;
+        });
+    }
+}
