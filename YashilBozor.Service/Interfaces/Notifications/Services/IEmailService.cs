9e02b6e4984553d301930b27ddea69d588bcc3ba
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-01-27 05:51:17 +0500

    Implemented Email service

---
+++ YashilBozor.Service/Interfaces/Notifications/Services/IEmailService.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using YashilBozor.Domain.Entities.Notification;
+
+namespace YashilBozor.Service.Interfaces.Notifications.Services;
+
+public interface IEmailService
+{
+    IQueryable<Email> Get(
+        Expression<Func<Email, bool>>? predicate = default,
+        bool asNoTracking = false);
+
+    ValueTask<Email?> GetByIdAsync(
+        Guid emailId,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Email> CreateAsync(
+        Email emailTemplate,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<Email> UpdateAsync(
+        Email email,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+}
---
+++ YashilBozor.Service/Services/Notifications/Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using YashilBozor.DAL.IRepositories.Notifications;
+using YashilBozor.Domain.Entities.Notification;
+using YashilBozor.Service.Interfaces.Notifications.Services;
+
+namespace YashilBozor.Service.Services.Notifications.Services;
+
+public class EmailService(IEmailRepository emailRepository) : IEmailService
+{
+    public IQueryable<Email> Get(
+        Expression<Func<Email, bool>>? predicate = default,
+        bool asNoTracking = false)
+    {
+        return emailRepository.SelectAll(predicate, asNoTracking);
+    }
+
+    public ValueTask<Email?> GetByIdAsync(
+        Guid emailId,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default)
+    {
+        return emailRepository.SelectByIdAsync(emailId, asNoTracking, cancellationToken);
+    }
+
+    public ValueTask<Email> CreateAsync(
+        Email email, 
+        bool saveChanges = true, 
+        CancellationToken cancellationToken = default)
+    {
+        return emailRepository.InsertAsync(email, saveChanges, cancellationToken);
+    }
+
+    public ValueTask<Email> UpdateAsync(
+        Email email, 
+        bool saveChanges = true, 
+        CancellationToken cancellationToken = default)
+    {
+        return emailRepository.UpdateAsync(email, saveChanges, cancellationToken);
+    }
+}
