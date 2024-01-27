94af7989a1d0abe4569aba3217a2de3056123b36
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-01-27 05:53:46 +0500

    Created repositories

---
+++ YashilBozor.DAL/IRepositories/Notifications/IEmailRepository.cs
@@ -0,0 +1,8 @@
+﻿using YashilBozor.DAL.IRepositories.Commons;
+using YashilBozor.Domain.Entities.Notification;
+
+namespace YashilBozor.DAL.IRepositories.Notifications;
+
+public interface IEmailRepository : IRepository<Email>
+{
+}
